95f04a394f0260646dd8fdd4ee16a292545866ea
littleossa <littleossa@gmail.com>
2022-08-24 09:23:06 +0900

    Initial Commit

---
+++ PopUpDialogViewSample/PopUpDialogViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpDialogViewSampleApp.swift
+//  PopUpDialogViewSample
+//
+//  Created by 平岡修 on 2022/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PopUpDialogViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
